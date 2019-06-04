08ee92e4628066181450ccc897b2b8c855d6b9e0
Elian Doran <elinnum@gmail.com>
2019-06-04 23:02:01 +0300

    Display humidity

---
+++ Weather/ValueConverters/RelativeHumidityValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Weather.Core.Units;
+
+namespace Weather.ValueConverters
+{
+    public class RelativeHumidityValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is RelativeHumidity))
+                return value;
+
+            var humidity = (RelativeHumidity)value;
+            return (int)(Math.Round(humidity.Percentage * 100)) + "%";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
