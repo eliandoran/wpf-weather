30d6e31fb1780d0239d2961164a570aca65a6d2e
Elian Doran <elinnum@gmail.com>
2019-06-04 23:18:24 +0300

    Display apparent temperature

---
+++ Weather/ValueConverters/TemperatureValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Weather.Core.Units;
+
+namespace Weather.ValueConverters
+{
+    public class TemperatureValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Temperature))
+                return value;
+
+            var temperature = (Temperature)value;
+            return Math.Round(temperature.Celsius) + "°C";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
