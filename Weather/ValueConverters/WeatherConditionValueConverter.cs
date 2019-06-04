4295fa572ba0ecd473ec3c8d08bae87680a66be2
Elian Doran <elinnum@gmail.com>
2019-06-05 00:01:54 +0300

    Display weather condition

---
+++ Weather/ValueConverters/WeatherConditionValueConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Weather.Core;
+
+namespace Weather.ValueConverters
+{
+    class WeatherConditionValueConverter : IValueConverter
+    {
+        private static readonly Dictionary<WeatherCondition, string> conditionMappings = new Dictionary<WeatherCondition, string>()
+        {
+            { WeatherCondition.BrokenClouds, "Broken clouds" },
+            { WeatherCondition.Clear, "Clear" },
+            { WeatherCondition.FewClouds, "Few clouds" },
+            { WeatherCondition.Mist, "Mist" },
+            { WeatherCondition.Rain, "Rain" },
+            { WeatherCondition.ScatteredClouds, "Scattered clouds" },
+            { WeatherCondition.ShowerRain, "Shower rain" },
+            { WeatherCondition.Snow, "Snow" },
+            { WeatherCondition.Thunderstorm, "Thunderstorm" }
+        };
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is WeatherCondition))
+                return value;
+
+            var condition = (WeatherCondition)value;
+            return conditionMappings[condition];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
