57ac18b9b1ca8ab7c32b9527e4bdb702f3e8d274
Elian Doran <elinnum@gmail.com>
2019-06-04 22:28:04 +0300

    Start implementing data binding

--- Weather/MainWindow.xaml.cs
+++ Weather/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Weather.Core;
+using Weather.Providers;
 
 namespace Weather
 {
@@ -20,8 +22,22 @@ namespace Weather
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly String weatherJsonString = "{\"coord\":{\"lon\":24.16,\"lat\":45.79},\"weather\":[{\"id\":801,\"main\":\"Clouds\",\"description\":\"few clouds\",\"icon\":\"02d\"}],\"base\":\"stations\",\"main\":{\"temp\":294.21,\"pressure\":1012,\"humidity\":60,\"temp_min\":294.15,\"temp_max\":294.26},\"visibility\":10000,\"wind\":{\"speed\":3.6,\"deg\":110},\"clouds\":{\"all\":20},\"dt\":1559663577,\"sys\":{\"type\":1,\"id\":6920,\"message\":0.0055,\"country\":\"RO\",\"sunrise\":1559615749,\"sunset\":1559671663},\"timezone\":10800,\"id\":667268,\"name\":\"Sibiu\",\"cod\":200}";
+
+        public static readonly DependencyProperty WeatherProperty = DependencyProperty.Register(
+            "Weather", typeof(WeatherDay), typeof(MainWindow));
+
+        public WeatherDay Weather
+        {
+            get { return (WeatherDay)GetValue(WeatherProperty); }
+            set { SetValue(WeatherProperty, value); }
+        }
+
         public MainWindow()
         {
+            var provider = new OpenWeatherMapProvider();
+            Weather = provider.ParseCurrentWeather(weatherJsonString);
+
             InitializeComponent();
         }
     }
