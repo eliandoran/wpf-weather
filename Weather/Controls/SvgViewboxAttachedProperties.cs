6fad9381cce111a44f6b66cac10c9a1e83e15462
Elian Doran <elinnum@gmail.com>
2019-06-05 11:12:56 +0300

    Display condition images in forecast

---
+++ Weather/Controls/SvgViewboxAttachedProperties.cs
@@ -0,0 +1,42 @@
+﻿using SharpVectors.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Weather.Controls
+{
+    /*
+     * Courtesy of https://stackoverflow.com/a/35088090.
+     */
+    public class SvgViewboxAttachedProperties: DependencyObject
+    {
+        public static string GetSource(DependencyObject obj)
+        {
+            return (string)obj.GetValue(SourceProperty);
+        }
+
+        public static void SetSource(DependencyObject obj, string value)
+        {
+            obj.SetValue(SourceProperty, value);
+        }
+
+        private static void OnSourceChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var svgControl = obj as SvgViewbox;
+            if (svgControl != null)
+            {
+                var path = (string)e.NewValue;
+                svgControl.Source = string.IsNullOrWhiteSpace(path) ? default(Uri) : new Uri(path);
+            }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.RegisterAttached("Source",
+                typeof(string), typeof(SvgViewboxAttachedProperties),
+                // default value: null
+                new PropertyMetadata(null, OnSourceChanged));
+    }
+}
---
+++ Weather/ValueConverters/WeatherConditionImageValueConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Weather.Core;
+
+namespace Weather.ValueConverters
+{
+    class WeatherConditionImageValueConverter : IValueConverter
+    {
+        private static readonly Dictionary<WeatherCondition, string> conditionMappings = new Dictionary<WeatherCondition, string>()
+        {
+            { WeatherCondition.BrokenClouds, "Cloud" },
+            { WeatherCondition.Clear, "Clear" },
+            { WeatherCondition.FewClouds, "CloudSun" },
+            { WeatherCondition.Mist, "Cloud" },
+            { WeatherCondition.ScatteredClouds, "CloudSun" },
+            { WeatherCondition.ShowerRain, "Rain" },
+            { WeatherCondition.Snow, "Snow" },
+            { WeatherCondition.Thunderstorm, "Thunderstorm" }
+        };
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is WeatherCondition))
+                return value;
+
+            var condition = (WeatherCondition)value;
+            var icon = conditionMappings[condition];
+
+            return "pack://application:,,,/Assets/" + icon + ".svg";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
