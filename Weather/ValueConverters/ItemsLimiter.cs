9579d368520448c28e1dd10d1e92f773b8a940be
Elian Doran <elinnum@gmail.com>
2019-06-05 09:43:47 +0300

    Display forecast for three days only

---
+++ Weather/ValueConverters/ItemsLimiter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Weather.ValueConverters
+{
+    public class ItemsLimiter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int count;
+
+            if (!int.TryParse(parameter.ToString(), out count) || value == null)
+                return value;
+
+            return ((IEnumerable<object>)value).Take(count);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
